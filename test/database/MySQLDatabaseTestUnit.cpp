d201de2310e6d59b5f23363d1e19bc3d686c619c
Sascha Kühl <sascha.kuehl@gmx.net>
2013-07-06 22:37:55 +0200

    fixed typo

--- test/database/MySQLDatabaseTestUnit.cpp
+++ test/database/MySQLDatabaseTestUnit.cpp
@@ -7,7 +7,7 @@ using namespace std;
 #define MYSQL_DSN "mysql://sascha@192.168.27.57/test"
 #else
 //#define MYSQL_DSN "mysql://sascha:sascha@localhost/test"
-#define MYSQL_DSN "mysql://root@l127.0.0.1/oos_test"
+#define MYSQL_DSN "mysql://root@127.0.0.1/oos_test"
 #endif
 
 MySQLDatabaseTestUnit::MySQLDatabaseTestUnit()
