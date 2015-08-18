7808a8597b5038a8478f352fb6ad56760055aebc
zussel <sascha.kuehl@gmx.net>
2015-08-18 14:57:35 +0200

    added prototype tree relation test

--- test/object/PrototypeTreeTest.cpp
+++ test/object/PrototypeTreeTest.cpp
@@ -22,6 +22,7 @@ PrototypeTreeTestUnit::PrototypeTreeTestUnit()
   add_test("child_of", std::bind(&PrototypeTreeTestUnit::test_child_of, this), "test child of element");
   add_test("traverse", std::bind(&PrototypeTreeTestUnit::test_traverse, this), "test traversing the prototype tree");
   add_test("const_traverse", std::bind(&PrototypeTreeTestUnit::test_const_traverse, this), "test const traversing the prototype tree");
+  add_test("relations", std::bind(&PrototypeTreeTestUnit::test_relations, this), "test relations");
 }
 
 PrototypeTreeTestUnit::~PrototypeTreeTestUnit()
@@ -260,4 +261,121 @@ void PrototypeTreeTestUnit::test_const_traverse()
   first--;
 
   UNIT_ASSERT_TRUE(first == ptree.begin(), "expected prototype iterator to be begin()");
+}
+
+void PrototypeTreeTestUnit::test_relations()
+{
+  prototype_tree ptree;
+
+  class child : public oos::serializable
+  {
+  public:
+      child() {}
+      child(const std::string &n) : name(n) {}
+      virtual ~child() {}
+
+      virtual void deserialize(object_reader &r)
+      {
+          r.read("id", id);
+          r.read("name", name);
+      }
+
+      virtual void serialize(object_writer &w) const
+      {
+          w.write("id", id);
+          w.write("name", name);
+      }
+
+      oos::primary_key<long> id;
+      std::string name;
+  };
+
+  class master : public oos::serializable
+  {
+  public:
+      master() {}
+      master(const std::string &n) : name(n) {}
+      virtual ~master() {}
+
+      virtual void deserialize(object_reader &r)
+      {
+          r.read("id", id);
+          r.read("name", name);
+          r.read("child", children);
+      }
+
+      virtual void serialize(object_writer &w) const
+      {
+          w.write("id", id);
+          w.write("name", name);
+          w.write("child", children);
+      }
+
+      oos::primary_key<long> id;
+      std::string name;
+      oos::object_ptr<child> children;
+  };
+
+  class children_list : public oos::serializable
+  {
+  public:
+      typedef oos::object_ref<child> child_ref;
+      typedef oos::object_list<children_list, child_ref, true> children_list_t;
+
+      children_list() {}
+      children_list(const std::string &n) : name(n) {}
+      virtual ~children_list() {}
+
+      virtual void deserialize(object_reader &r)
+      {
+          r.read("id", id);
+          r.read("name", name);
+          r.read("children", children);
+      }
+
+      virtual void serialize(object_writer &w) const
+      {
+          w.write("id", id);
+          w.write("name", name);
+          w.write("children", children);
+      }
+
+      oos::primary_key<long> id;
+      std::string name;
+      children_list_t children;
+  };
+  prototype_tree::const_iterator child_node = ptree.insert<child>("child");
+  prototype_tree::const_iterator master_node = ptree.insert<master>("master");
+  prototype_tree::const_iterator children_list_node = ptree.insert<children_list>("children_list");
+
+  /*
+   * get the relation table for children
+   * holding id of children_list (container)
+   * and id of child (value)
+   */
+  prototype_tree::const_iterator children_node = ptree.find("children");
+
+  UNIT_ASSERT_TRUE(child_node->relations.empty(), "there must be no relations");
+  UNIT_ASSERT_TRUE(child_node->foreign_keys.empty(), "there must be no foreign keys");
+
+  UNIT_ASSERT_TRUE(children_list_node->relations.empty(), "there must be no relations");
+  UNIT_ASSERT_TRUE(children_list_node->foreign_keys.empty(), "there must be no foreign keys");
+
+  UNIT_ASSERT_TRUE(master_node->relations.empty(), "there must be no relations");
+  UNIT_ASSERT_FALSE(master_node->foreign_keys.empty(), "there must be one foreign key");
+  UNIT_ASSERT_EQUAL(master_node->foreign_keys.size(), (size_t)1, "there must be one foreign key");
+  prototype_node::t_foreign_key_map::const_iterator j = master_node->foreign_keys.find("child");
+  UNIT_ASSERT_FALSE(j == master_node->foreign_keys.end(), "iterator must not be end");
+
+  UNIT_ASSERT_FALSE(children_node->relations.empty(), "there must be one relation");
+  UNIT_ASSERT_EQUAL(children_node->relations.size(), (size_t)1, "there must be one relation");
+  prototype_node::field_prototype_map_t::const_iterator i = children_node->relations.find("children_list");
+  UNIT_ASSERT_FALSE(i == children_node->relations.end(), "iterator must not be end");
+
+  UNIT_ASSERT_FALSE(children_node->foreign_keys.empty(), "there must be one foreign key");
+  UNIT_ASSERT_EQUAL(children_node->foreign_keys.size(), (size_t)2, "there must be two foreign keys");
+  j = children_node->foreign_keys.find("container");
+  UNIT_ASSERT_FALSE(j == children_node->foreign_keys.end(), "iterator must not be end");
+  j = children_node->foreign_keys.find("value");
+  UNIT_ASSERT_FALSE(j == children_node->foreign_keys.end(), "iterator must not be end");
 }
