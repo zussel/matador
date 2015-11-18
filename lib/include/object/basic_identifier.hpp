9045768138bf4216ce5f2d6ea3ff9ab1386dda15
Sascha Kühl <sascha.kuehl@gmx.net>
2015-11-18 22:37:56 +0100

    documentation progress

--- lib/include/object/basic_identifier.hpp
+++ lib/include/object/basic_identifier.hpp
@@ -103,14 +103,50 @@ public:
    */
   virtual bool equal_to(const basic_identifier &x) const = 0;
 
+  /**
+   * Returns a unique hash number
+   *
+   * @return A unique hash number
+   */
   virtual size_t hash() const = 0;
 
+  /**
+   * Returns true if the given identifier
+   * is of the same type as this identifier
+   *
+   * @param x Identifier to compare the type with
+   * @return True if their types are equal
+   */
   virtual bool is_same_type(const basic_identifier &x) const = 0;
-  virtual const std::type_index &type_index() const = 0;
 
+  /**
+   * Clone this identifier
+   *
+   * @return The cloned identifier
+   */
   virtual basic_identifier *clone() const = 0;
+
+  /**
+   * Share the id of the identifier. The underlying
+   * concrete id of an identifier is a shared object
+   * so it can be shared within more identifiers
+   * of the same type
+   *
+   * @return A new identifier object with the shared id.
+   */
   virtual basic_identifier* share() = 0;
+
+  /**
+   * Isolate means unshring a shared identifier id creating
+   * a new copy of the identifier id.
+   */
   virtual void isolate() = 0;
+
+  /**
+   * Share identifiers id with given identifier.
+   *
+   * @param id The identifier to share the id with.
+   */
   virtual void share_with(basic_identifier &id) = 0;
 
   virtual std::ostream &print(std::ostream &out) const = 0;
@@ -121,6 +157,14 @@ public:
 
   template<class T>
   T id() const;
+
+protected:
+  /**
+   * The returns the type_index of a specific type
+   *
+   * @return The specific type_index
+   */
+  virtual const std::type_index &type_index() const = 0;
 };
 
 template<class T>
