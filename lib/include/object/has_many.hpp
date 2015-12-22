97f728fd979e65b5bb0c7f5178d05b6c0a6d2b57
zussel <sascha.kuehl@gmx.net>
2015-12-22 14:04:30 +0100

    has_many progress

--- lib/include/object/has_many.hpp
+++ lib/include/object/has_many.hpp
@@ -33,20 +33,78 @@ protected:
 template < class T, class O, template <class ...> class C = std::vector, class Enable = void >
 class has_many;
 
+/**
+ * @class has_many_item
+ * @brief Holds item and owner id of a has many relation
+ * @tparam T Type of the item
+ */
+template < class T >
+class has_many_item
+{
+public:
+
+  has_many_item()
+    : owner_id_("owner_id")
+    , item_id_("item_id")
+  { }
+
+  has_many_item(const std::string &owner_id, const std::string &item_id)
+    : owner_id_(owner_id)
+    , item_id_(item_id)
+  { }
+
+  template < class SERIALIZER >
+  void serialize(SERIALIZER &serializer)
+  {
+    serializer.serialize(owner_id_.c_str(), owner_);
+    serializer.serialize(item_id_.c_str(), item_);
+  }
+
+
+  std::string owner_id() const
+  {
+    return owner_id_;
+  }
+
+  void owner_id(const std::string &oid)
+  {
+    owner_id_ = oid;
+  }
+
+  std::string item_id() const
+  {
+    return item_id_;
+  }
+
+  void item_id(const std::string &iid)
+  {
+    item_id_ = iid;
+  }
+
+private:
+  basic_identifier owner_;
+  has_one<T> item_;
+
+  std::string owner_id_;
+  std::string item_id_;
+};
+
 template < class T, class O, template <class ...> class C >
 class has_many<T, C, typename std::enable_if<is_same_container_type<C, std::vector>::value || is_same_container_type<C, std::list>::value>::type> : public basic_has_many<T>
 {
 public:
 
-  typedef oos::has_one<T> value_type;
-  typedef C<oos::has_one<T>, std::allocator<oos::has_one<T>>> container_type;
+  typedef has_many_item<T> value_type;
+//  typedef oos::has_one<T> value_type;
+  typedef C<value_type, std::allocator<value_type>> container_type;
+//  typedef C<oos::has_one<T>, std::allocator<oos::has_one<T>>> container_type;
   typedef typename container_type::size_type size_type;
   typedef typename container_type::iterator iterator;
   typedef typename container_type::const_iterator const_iterator;
 
-  void push_back(const value_type &value)
+  void push_back(const oos::object_ptr<T> &value)
   {
-    container_.push_back(value);
+//    container_.push_back(value);
   }
 
   iterator begin() { return container_.begin(); }
