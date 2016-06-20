c0dc5c3f34984f4f8c27bf70c2d45264473615e6
Sascha Kühl <sascha.kuehl@gmx.net>
2016-06-20 07:59:56 +0200

    sqlite dialect compiler progress

---
+++ db/sqlite/include/sqlite_dialect_compiler.hpp
@@ -0,0 +1,22 @@
+//
+// Created by sascha on 17.06.16.
+//
+
+#ifndef OOS_SQLITE_DIALECT_COMPILER_HPP
+#define OOS_SQLITE_DIALECT_COMPILER_HPP
+
+#include "sql/basic_dialect_compiler.hpp"
+
+namespace oos {
+
+namespace sqlite {
+
+class sqlite_dialect_compiler : public detail::basic_dialect_compiler
+{
+
+};
+
+}
+}
+
+#endif //OOS_SQLITE_DIALECT_COMPILER_HPP
