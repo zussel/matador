60a6d855d42d6cad45f285f7b98d922a04205c10
Sascha Kühl <sascha.kuehl@gmx.net>
2016-09-14 18:51:32 +0200

    doxygen update

--- api/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sascha/development/oos/doc
-# Build directory: /home/sascha/development/oos/build/debug/doc
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
