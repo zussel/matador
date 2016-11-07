b509b63975a3383aebd10ad15cb4ea4b3dd9517d
Sascha Kühl <sascha.kuehl@gmx.net>
2016-11-07 06:46:52 +0100

    mssql test progress

--- db/mssql/src/mssql_connection.cpp
+++ db/mssql/src/mssql_connection.cpp
@@ -104,6 +104,8 @@ void mssql_connection::open(const std::string &connection)
 
   std::string dns("DRIVER={" + driver + "};SERVER=" + host + ";Protocol=TCPIP;Port=1433;DATABASE=" + db_ + ";UID=" + user + ";PWD=" + passwd + ";");
 
+  std::cout << "SQL: " << dns << "\n";
+
   SQLCHAR retconstring[1024];
   ret = SQLDriverConnect(connection_, 0, (SQLCHAR*)dns.c_str(), SQL_NTS, retconstring, 1024, NULL,SQL_DRIVER_NOPROMPT);
 
