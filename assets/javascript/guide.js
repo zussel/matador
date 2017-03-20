016e0b9893a897717880e188b922beaca7ab9944
Sascha Kühl <sascha.kuehl@gmx.net>
2017-03-20 16:37:28 +0100

    scrollspy and navigation

--- assets/javascript/guide.js
+++ assets/javascript/guide.js
@@ -6,5 +6,11 @@
 //     }
 //   }
 // });
+$(':header').each(function() {
+    var id = $(this).attr('id');
+    $(this).removeAttr('id');
+    $(this).prepend("<a class=\"anchor\" id=\"" + id + "\"></a>");
+});
+// $(':header').prepend("<a class=\"anchor\"></a>");
 $('#markdown-toc ul').addClass('nav nav-stacked');
 $('body').scrollspy({ target: '.bs-docs-sidebar', offset: 80 });
