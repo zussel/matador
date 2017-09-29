f5b42ce6561a7f5f7d60be713839f77a71f6dda3
zussel <sascha.kuehl@gmx.net>
2017-09-29 17:30:13 +0200

    relation data progress

---
+++ include/matador/object/relation_data.hpp
@@ -0,0 +1,29 @@
+//
+// Created by sascha on 9/29/17.
+//
+
+#ifndef MATADOR_RELATION_DATA_HPP
+#define MATADOR_RELATION_DATA_HPP
+
+namespace matador {
+
+namespace detail {
+
+class relation_data
+{
+
+};
+
+class relation_has_many_data : public relation_data
+{
+
+};
+
+class relation_has_one_data : public relation_data
+{
+
+};
+
+}
+}
+#endif //MATADOR_RELATION_DATA_HPP
