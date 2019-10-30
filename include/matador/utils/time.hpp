18385aac90c9203d1d95df6000a82b993acf6fd7
zussel <sascha.kuehl@gmx.net>
2019-10-30 22:52:13 +0100

    revert tm initialization

--- include/matador/utils/time.hpp
+++ include/matador/utils/time.hpp
@@ -428,7 +428,8 @@ private:
 
 private:
   struct timeval time_ = {0,0 };
-  struct tm tm_ = {0,0,0,0,0,0,0,0,0, 0, nullptr };
+//  struct tm tm_ = {0,0,0,0,0,0,0,0,0,0,nullptr};
+  struct tm tm_ = {};
 };
 
 }
