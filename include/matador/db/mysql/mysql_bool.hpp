78a8329b9369c8b079b9b96d940992c513eb2757
Sascha Kühl <skuehl1972@gmail.com>
2020-01-05 13:34:53 +0100

    added missing include

--- include/matador/db/mysql/mysql_bool.hpp
+++ include/matador/db/mysql/mysql_bool.hpp
@@ -5,6 +5,12 @@
 #ifndef MATADOR_MYSQL_BOOL_HPP
 #define MATADOR_MYSQL_BOOL_HPP
 
+#ifdef _MSC_VER
+#include <mysql.h>
+#else
+#include <mysql/mysql.h>
+#endif
+
 #if !MARIADB_PACKAGE_VERSION_ID && MYSQL_VERSION_ID >= 80001
 typedef bool my_bool;
 #endif
