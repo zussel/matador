491b197303fc793a7283bac5ba70916bbfa9378e
Sascha Kühl <skuehl1972@gmail.com>
2021-05-10 15:16:09 +0200

    add network initializations to demo target

--- examples/demo/main.cpp
+++ examples/demo/main.cpp
@@ -37,6 +37,8 @@ int main(int /*argc*/, char* /*argv*/[])
   matador::add_log_sink(matador::create_file_sink("log/server.log"));
   matador::add_log_sink(matador::create_stdout_sink());
 
+  net::init();
+
   http::server server(19082);
   server.add_routing_middleware();
   server.add_middleware(std::make_shared<authentication_middleware>());
@@ -50,6 +52,8 @@ int main(int /*argc*/, char* /*argv*/[])
   main_page mpage(server);
 
   server.run();
+
+  net::cleanup();
 
   return 0;
 }
