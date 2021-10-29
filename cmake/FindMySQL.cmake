92c436fa05f31208cc4355a8e714b3930e3334af
Sascha Kühl <skuehl1972@gmail.com>
2021-10-29 10:39:45 +0200

    fixed cmake find mysql

--- cmake/FindMySQL.cmake
+++ cmake/FindMySQL.cmake
@@ -2,48 +2,26 @@
 # Find the native MySQL includes and library
 #
 #  MYSQL_INCLUDE_DIR - where to find mysql.h, etc.
-#  MYSQL_LIBRARIES   - List of libraries when using MySQL.
+#  MYSQL_LIBRARY   - List of libraries when using MySQL.
 #  MYSQL_FOUND       - True if MySQL found.
 
 IF (MYSQL_INCLUDE_DIR)
   # Already in cache, be silent
   SET(MYSQL_FIND_QUIETLY TRUE)
-  MESSAGE(STATUS "MYSQL_INCLUDE_DIR ${MYSQL_INCLUDE_DIR}")
 ENDIF (MYSQL_INCLUDE_DIR)
 
 if(WIN32)
-  MESSAGE(STATUS "MYSQL_DIR $ENV{MYSQL_DIR}")
-
-  MESSAGE(STATUS "Checking $ENV{MYSQL_DIR}/include/mysql.h")
-  if(EXISTS "$ENV{MYSQL_DIR}/include/mysql.h")
-    MESSAGE(STATUS "MySQL header exists")
-  else()
-    MESSAGE(STATUS "MySQL header doesn't exists")
-  endif()
-
-  MESSAGE(STATUS "Checking $ENV{MYSQL_DIR}/lib/libmysql.dll")
-  if(EXISTS "$ENV{MYSQL_DIR}/lib/libmysql.dll")
-    MESSAGE(STATUS "MySQL lib exists")
-  else()
-    MESSAGE(STATUS "MySQL lib doesn't exists")
-  endif()
-
-endif(WIN32)
-
-if(WIN32)
-  MESSAGE(STATUS "Searching for MySQL include in $ENV{ProgramFiles}")
   find_path(MYSQL_INCLUDE_DIR mysql.h
     PATHS
-#    $ENV{MYSQL_INCLUDE_DIR}
+    $ENV{MYSQL_INCLUDE_DIR}
     $ENV{MYSQL_DIR}/include
     $ENV{ProgramFiles}/MySQL/*/include
-#    $ENV{SystemDrive}/MySQL/*/include
-#    $ENV{ProgramW6432}/MySQL/*/include
-#    $ENV{ProgramFiles}/MariaDB/*/include
-#    $ENV{SystemDrive}/MariaDB/*/include
-#    $ENV{ProgramW6432}/MariaDB/*/include
+    $ENV{SystemDrive}/MySQL/*/include
+    $ENV{ProgramW6432}/MySQL/*/include
+    $ENV{ProgramFiles}/MariaDB/*/include
+    $ENV{SystemDrive}/MariaDB/*/include
+    $ENV{ProgramW6432}/MariaDB/*/include
    )
-  MESSAGE(STATUS "Found MYSQL_INCLUDE_DIR ${MYSQL_INCLUDE_DIR}")
 else(WIN32)
   find_path(MYSQL_INCLUDE_DIR mysql.h
     PATHS
@@ -81,6 +59,11 @@ if(WIN32)
 #    $ENV{MYSQL_DIR}/client/${build_dist}
 #    $ENV{ProgramFiles}/MySQL/*/lib/${binary_dist}
     $ENV{ProgramFiles}/MySQL/*/lib
+    $ENV{SystemDrive}/MySQL/*/lib
+    $ENV{ProgramW6432}/MySQL/*/lib
+    $ENV{ProgramFiles}/MariaDB/*/lib
+    $ENV{SystemDrive}/MariaDB/*/lib
+    $ENV{ProgramW6432}/MariaDB/*/lib
 #    $ENV{SystemDrive}/MySQL/*/lib/${binary_dist}
 #    $ENV{ProgramFiles}/MariaDB/*/lib/${binary_dist}
 #    $ENV{SystemDrive}/MariaDB/*/lib/${binary_dist}
@@ -129,19 +112,18 @@ ELSE (MYSQL_INCLUDE_DIR)
   MESSAGE(STATUS "MariaDB include dir not found")
 ENDIF (MYSQL_INCLUDE_DIR)
 
-IF (MYSQL_LIBRARIES)
-  MESSAGE(STATUS "MariaDB libs ${MYSQL_LIBRARIES}")
-ELSE (MYSQL_LIBRARIES)
+IF (MYSQL_LIBRARY)
+  MESSAGE(STATUS "MariaDB libs ${MYSQL_LIBRARY}")
+ELSE (MYSQL_LIBRARY)
   MESSAGE(STATUS "MariaDB libs dir not found")
-ENDIF (MYSQL_LIBRARIES)
+ENDIF (MYSQL_LIBRARY)
 
 
 IF (MYSQL_INCLUDE_DIR AND MYSQL_LIBRARY)
   SET(MYSQL_FOUND TRUE)
-  SET( MYSQL_LIBRARIES ${MYSQL_LIBRARY} )
 ELSE (MYSQL_INCLUDE_DIR AND MYSQL_LIBRARY)
   SET(MYSQL_FOUND FALSE)
-  SET( MYSQL_LIBRARIES )
+  SET(MYSQL_LIBRARY)
 ENDIF (MYSQL_INCLUDE_DIR AND MYSQL_LIBRARY)
 
 MARK_AS_ADVANCED(MYSQL_LIBRARY MYSQL_INCLUDE_DIR)
