1e1a648874155925564b69dc1cc0555ae98f246f
Sascha Kühl <skuehl1972@gmail.com>
2024-10-04 15:51:11 +0200

    added tests for custom error result

--- test/catch2/utils/ResultTest.cpp
+++ test/catch2/utils/ResultTest.cpp
@@ -1,9 +1,12 @@
+#include <utility>
+
 #include "_deps/catch2-src/src/catch2/catch_test_macros.hpp"
 
 #include "matador/utils/result.hpp"
 
 namespace matador::test {
 enum class math_error : int32_t {
+  OK = 0,
   DIVISION_BY_ZERO = 1,
   FAILURE = 2
 };
@@ -120,4 +123,46 @@ TEST_CASE("Result tests", "[result]") {
   REQUIRE(res_void);
   REQUIRE(res_void.is_ok());
   REQUIRE(!res_void.is_error());
+}
+
+namespace matador::test {
+class CustomError {
+public:
+    CustomError( const math_error err, std::string msg)
+    : error_(err), message_(std::move(msg)) {}
+
+    [[nodiscard]] math_error error() const { return error_; }
+    [[nodiscard]] std::string message() const { return message_; }
+
+private:
+    math_error error_{};
+    std::string message_;
+};
+
+utils::result<float, CustomError> custom_divide(const float x, const float y) {
+    utils::result<float, math_error> res = divide(x, y);
+
+    if (res.is_ok()) {
+        return utils::ok(res.value());
+    }
+
+    return utils::error<CustomError>({res.err(), "ERROR"});
+}
+}
+
+TEST_CASE("Result tests with custom error", "[result][custom]") {
+    auto res = test::custom_divide(4, 2);
+
+    REQUIRE(res.is_ok());
+    REQUIRE(!res.is_error());
+
+    res = test::custom_divide(4, 0);
+
+    REQUIRE(!res.is_ok());
+    REQUIRE(res.is_error());
+
+    const auto err = res.err();
+
+    REQUIRE(err.error() == test::math_error::DIVISION_BY_ZERO);
+    REQUIRE(err.message() == "ERROR");
 }
