33fcc9e3fbbd39fb47fce0d927f88e8390c0c2c4
Sascha Kühl <skuehl1972@gmail.com>
2024-10-31 16:04:26 +0100

    refactored to_value.hpp using std::from_chars

--- include/matador/sql/to_value.hpp
+++ include/matador/sql/to_value.hpp
@@ -1,12 +1,10 @@
 #ifndef QUERY_TO_VALUE_HPP
 #define QUERY_TO_VALUE_HPP
 
-#include <cerrno>
-#include <climits>
+#include <charconv>
 #include <cstring>
-#include <cfloat>
-#include <cstdlib>
 #include <stdexcept>
+#include <string>
 #include <type_traits>
 
 namespace matador::sql {
@@ -16,69 +14,40 @@ void to_value(bool &value, const char *str);
 template < class Type >
 void to_value(Type &value, const char *str, std::enable_if_t<std::is_integral_v<Type> && std::is_signed_v<Type>>* = nullptr)
 {
-  if (strlen(str) == 0) {
-    return;
-  }
-  char *end;
-  errno = 0;
-  auto result = strtoll(str, &end, 10);
+  const auto res = std::from_chars(str, str + strlen(str), value);
 
-  // Check for various possible errors
-  if ((errno == ERANGE && (result == LLONG_MAX || result == LLONG_MIN)) || (errno != 0 && result == 0)) {
-    throw std::logic_error(strerror(errno));
-    // Handle error
-  } else if (end == str) {
-    // No digits found
+  if (res.ec == std::errc::invalid_argument) {
     throw std::logic_error("failed to convert value to signed number: no digits were found");
   }
-
-  value = static_cast<Type>(result);
+  if (res.ec == std::errc::result_out_of_range) {
+    throw std::logic_error(std::string("out of range! res.ptr distance: ") + std::to_string(res.ptr - str));
+  }
 }
 
 template < class Type >
 void to_value(Type &value, const char *str, std::enable_if_t<std::is_integral_v<Type> && std::is_unsigned_v<Type> && !std::is_same_v<Type, bool>>* = nullptr)
 {
-  if (str == nullptr || strlen(str) == 0) {
-    return;
-  }
+    const auto res = std::from_chars(str, str + strlen(str), value);
 
-  char *end;
-  errno = 0;
-  auto result = strtoull(str, &end, 10);
-
-  // Check for various possible errors
-  if ((errno == ERANGE && (result == LLONG_MAX || result == LLONG_MIN)) || (errno != 0 && result == 0)) {
-    throw std::logic_error(strerror(errno));
-    // Handle error
-  } else if (end == str) {
-    // No digits found
-    throw std::logic_error("failed to convert value to unsigned number: no digits were found");
-  }
-
-  value = static_cast<Type>(result);
+    if (res.ec == std::errc::invalid_argument) {
+        throw std::logic_error("failed to convert value to unsigned number: no digits were found");
+    }
+    if (res.ec == std::errc::result_out_of_range) {
+        throw std::logic_error(std::string("out of range! res.ptr distance: ") + std::to_string(res.ptr - str));
+    }
 }
 
 template < class Type >
 void to_value(Type &value, const char *str, std::enable_if_t<std::is_floating_point_v<Type>>* = nullptr)
 {
-  if (strlen(str) == 0) {
-    return;
-  }
+  const auto res = std::from_chars(str, str + strlen(str), value, std::chars_format::general);
 
-  char *end;
-  errno = 0;
-  auto result = strtold(str, &end);
-
-  // Check for various possible errors
-  if ((errno == ERANGE && (result == LDBL_MAX || result == LDBL_MIN)) || (errno != 0 && result == 0)) {
-    throw std::logic_error(strerror(errno));
-    // Handle error
-  } else if (end == str) {
-    // No digits found
+  if (res.ec == std::errc::invalid_argument) {
     throw std::logic_error("failed to convert value to floating point number: no digits were found");
   }
-
-  value = static_cast<Type>(result);
+  if (res.ec == std::errc::result_out_of_range) {
+    throw std::logic_error(std::string("out of range! res.ptr distance: ") + std::to_string(res.ptr - str));
+  }
 }
 
 }
