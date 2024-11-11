ea8f8e8e25cc76da7fa0b458959e50ba71ea154a
Sascha Kuehl <skuehl1972@gmail.com>
2024-11-11 07:14:06 +0100

    statement cache progress

--- backends/sqlite/src/sqlite_dialect.cpp
+++ backends/sqlite/src/sqlite_dialect.cpp
@@ -1,12 +1,10 @@
 #include "sqlite_dialect.hpp"
-#include "sqlite_query_compiler.hpp"
 
 #include "matador/sql/dialect_builder.hpp"
 
 [[maybe_unused]] const matador::sql::dialect *get_dialect()
 {
   using namespace matador::sql;
-  using namespace matador::backends::sqlite;
   const static dialect d = dialect_builder::builder()
     .create()
     .with_token_replace_map({
@@ -15,7 +13,6 @@
                               {dialect_token::ROLLBACK, "ROLLBACK TRANSACTION"}
                             })
     .with_default_schema_name("main")
-    .with_compiler(std::make_unique<sqlite_query_compiler>())
     .build();
 
   return &d;
