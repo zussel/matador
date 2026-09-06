4d3ca0c3a51fb568c31bdc52354b71b1d845e9e0
Sascha Kühl <skuehl1972@gmail.com>
2026-09-06 22:08:40 +0200

    renamed query_create_intermediate to create_intermediate

--- include/matador/query/intermediates/create_intermediate.hpp
+++ include/matador/query/intermediates/create_intermediate.hpp
@@ -10,7 +10,7 @@
 
 namespace matador::query {
 class table;
-class query_create_table_columns_intermediate : public executable_query {
+class create_table_columns_intermediate : public executable_query {
 public:
   using executable_query::executable_query;
 
@@ -18,13 +18,13 @@ public:
   executable_query constraints(const std::list<constraint> &constraints);
 };
 
-class query_create_table_intermediate : public intermediate {
+class create_table_intermediate : public intermediate {
 public:
   using intermediate::intermediate;
 
-  query_create_table_columns_intermediate columns(std::initializer_list<column> cols);
-  query_create_table_columns_intermediate columns(const std::list<column> &cols);
-  query_create_table_columns_intermediate columns(const std::vector<column> &cols);
+  create_table_columns_intermediate columns(std::initializer_list<column> cols);
+  create_table_columns_intermediate columns(const std::list<column> &cols);
+  create_table_columns_intermediate columns(const std::vector<column> &cols);
 };
 
 class query_create_sequence_intermediate : public executable_query {
@@ -32,11 +32,11 @@ public:
   using executable_query::executable_query;
 };
 
-class query_create_intermediate : public intermediate {
+class create_intermediate : public intermediate {
 public:
-  query_create_intermediate();
+  create_intermediate();
 
-  query_create_table_intermediate table(const table &tab);
+  create_table_intermediate table(const table &tab);
   query_create_sequence_intermediate sequence(const std::string &sequence_name);
   executable_query schema(const std::string &schema_name);
 };
--- src/query/intermediates/create_intermediate.cpp
+++ src/query/intermediates/create_intermediate.cpp
@@ -1,4 +1,4 @@
-#include "matador/query/intermediates/query_create_intermediate.hpp"
+#include "matador/query/intermediates/create_intermediate.hpp"
 
 #include "matador/query/internal/query_parts.hpp"
 #include "matador/query/query_data.hpp"
@@ -7,44 +7,44 @@
 
 namespace matador::query {
 
-query_create_intermediate::query_create_intermediate() {
+create_intermediate::create_intermediate() {
   context_->parts.push_back(std::make_unique<internal::query_create_part>());
 }
 
-query_create_table_intermediate query_create_intermediate::table(const class table &tab) {
+create_table_intermediate create_intermediate::table(const class table &tab) {
   context_->parts.push_back(std::make_unique<internal::query_create_table_part>(tab));
   return {context_};
 }
 
-query_create_sequence_intermediate query_create_intermediate::sequence(const std::string& sequence_name) {
+query_create_sequence_intermediate create_intermediate::sequence(const std::string& sequence_name) {
   context_->parts.push_back(std::make_unique<internal::query_create_sequence_part>(sequence_name));
   return {context_};
 }
 
-executable_query query_create_intermediate::schema( const std::string& schema_name ) {
+executable_query create_intermediate::schema( const std::string& schema_name ) {
   context_->parts.push_back(std::make_unique<internal::query_create_schema_part>(schema_name));
   return {context_};
 }
 
-executable_query query_create_table_columns_intermediate::constraints(const std::initializer_list<constraint> constraints) {
+executable_query create_table_columns_intermediate::constraints(const std::initializer_list<constraint> constraints) {
   return this->constraints(std::list(constraints));
 }
 
-executable_query query_create_table_columns_intermediate::constraints(const std::list<constraint>& constraints){
+executable_query create_table_columns_intermediate::constraints(const std::list<constraint>& constraints){
   context_->parts.push_back(std::make_unique<internal::query_create_table_constraints_part>(constraints));
   return {context_};
 }
 
-query_create_table_columns_intermediate query_create_table_intermediate::columns(const std::initializer_list<column> cols) {
+create_table_columns_intermediate create_table_intermediate::columns(const std::initializer_list<column> cols) {
   return columns(std::vector(cols));
 }
 
-query_create_table_columns_intermediate query_create_table_intermediate::columns(const std::list<column> &cols) {
+create_table_columns_intermediate create_table_intermediate::columns(const std::list<column> &cols) {
   context_->parts.push_back(std::make_unique<internal::query_create_table_columns_part>(cols));
   return {context_};
 }
 
-query_create_table_columns_intermediate query_create_table_intermediate::columns(const std::vector<column>& cols) {
+create_table_columns_intermediate create_table_intermediate::columns(const std::vector<column>& cols) {
   std::list<column> columns;
   for (const auto& col : cols) {
     columns.emplace_back(col);
